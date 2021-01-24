e36fdcec789af4675dea4f6c3d9e81486519f529
Eoin Mag Ualghairg <77682134+eoiniritium@users.noreply.github.com>
2021-01-24 22:42:35 +0000

    finished re-writing in class

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ import pyglet  # better audio output
 
 # INITS
 
+listener = sr.Recognizer()
+engine = tts.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
 
 list_1 = []
 try:
@@ -19,14 +23,17 @@ except:
     pass
     print("Failed to initialize\ninsure that 'config.txt' is installed in the same folder as this script")
 
+
 def play_sound(filePath):
     sound = pyglet.resource.media(filePath)
     sound.play()
     pyglet.app.run()
 
+
 def getTextFile(file):
     f = open(file, "r")
     return f.read()
+
 
 def say(string):
     engine.say(string)
@@ -34,58 +41,59 @@ def say(string):
     print("DK why not working. Get main code working first")
 
 
+def runAthena(self):
+    command = Athena.sampleAudio(self)
+    if "play" in command:
+        Athena.play(self, command)
+    elif "time" in command:
+        Athena.time(self, command)
+    elif "define" in command:
+        Athena.define(self, command)
+    elif "search wikipedia for" in command:
+        Athena.searchWikipedia(self, command)
+    elif "what does" in command:
+        Athena.whatdoes(self, command)
+    elif "bitesize" in command or "bite size" in command:
+        Athena.bitesize(self, command)
+    elif "research" in command:
+        Athena.reaserch(self, command)
+
+
 class Athena:
-    def sampleAudio():
+    def sampleAudio(self):
         if not sr.Microphone:
             print("Cannot access microphone. See requirements.txt to install PyAudio.")
         with sr.Microphone() as source:
-            listener = sr.Recognizer()
-            engine = tts.init()
-            voices = engine.getProperty("voices")
-            engine.setProperty("voice", voices[1].id)
             print("listening...")
             voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
+            try:
+                command = listener.recognize_google(voice)
+            except:
+                command = "There was an error with the google recog. Probs one off error just loop and try again"
+            finally:
+                command = command.lower()
 
             for x in list_1:
-                #Debug line for testing what it hears vs the wake words
-                #print(x + " : " + command)
-                if x in command:
+                # Debug line for testing what it hears vs the wake words
+                # print(x + " : " + command)
+                if x in command or command in x:
                     print("woken...")
                     command = command.replace(x, "")
                     return command
 
-    def runAthena():
-        command = Athena.sampleAudio()
-        if "play" in command:
-            Athena.play(command)
-        elif "time" in command:
-            Athena.time(command)
-        elif "define" in command:
-            Athena.define(command)
-        elif "search wikipedia for" in command:
-            Athena.searchWikipedia(command)
-        elif "what does" in command:
-            Athena.whatdoes(command)
-        elif "bitesize" in command or "bite size" in command:
-            Athena.bitesize(command)
-        elif "research" in command:
-            Athena.reaserch(command)
-            
-    #FUNCS for what to happen (#DONE was for me, Eoin, to track proggress)
-    def play(input): #DONE
+    # FUNCS for what to happen (#DONE was for me, Eoin, to track proggress)
+    def play(self, input):  # DONE
         song = input.replace("play", "")
         print("playing" + song)
         say("playing" + song)
         pwk.playonyt(song)
 
-    def time(input): #DONE
+    def time(self, input):  # DONE
         time = datetime.datetime.now().strftime("%I:%M %p")
         print(time)
         say("the time is " + time)
 
-    def define(input): #DONE
+    def define(self, input):  # DONE
         thing = input.replace("define", "")
         thing = thing.replace("tofind", "")
         thing = thing.replace(" ", "")
@@ -96,7 +104,7 @@ class Athena:
         except:
             say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
 
-    def searchWikipedia(input): #DONE
+    def searchWikipedia(self, input):  # DONE
         thing = input.replace("search wikipedia for", "")
         thing = thing.replace("tofind", "")
         print(thing)
@@ -106,7 +114,7 @@ class Athena:
         except:
             say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
 
-    def whatdoes(input): #DONE
+    def whatdoes(self, input):  # DONE
         thing = input.replace("what does", "")
         thing = thing.replace("tofind", "")
         thing = thing.replace("mean", "")
@@ -117,23 +125,26 @@ class Athena:
         except:
             say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
             pass
-    #elif "repeat" in command:
-        say(command.replace("repeat", ""))
 
-    def bitesize(): #DONE
+    # elif "repeat" in command:
+    # say(command.replace("repeat", ""))
 
-        search = command.replace("bitesize", "")
+    def bitesize(self, input):  # DONE
+
+        search = input.replace("bitesize", "")
         search = search.replace(" ", "+")
         search = search.replace("++", "")
         print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
         say(("searching bbc bite size for %s" % search).replace("+", ""))
         wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
-    def reaserch(input):
-        search = command.replace("research", "")
+
+    def reaserch(self, input):
+        search = input.replace("research", "")
         search = search.replace(" ", "+")
         print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
         say(("searching bbc bite size for %s" % search).replace("+", ""))
         wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
 
+
 while True:
-    Athena.runAthena()
+    runAthena(self=Athena())
