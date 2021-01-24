1a16b7267779dee0ddead6c192ca6957e315697a
Eoin Mag Ualghairg <77682134+eoiniritium@users.noreply.github.com>
2021-01-24 00:43:41 +0000

    chagned to a class based system for ease of integration with front end

--- main.py
+++ main.py
@@ -28,12 +28,18 @@ def say(string):
     engine.runAndWait()
 
 
-def take_command():
-    list_1 = []
-    try:
-        with open("config.txt") as f:
-            for line in f:
-                list_1.append(line.strip('\n\r'))
+class Athena:
+    def __init__(self):
+        self.list_1 = []
+        try:
+            with open("config.txt") as f:
+                for line in f:
+                    list_1.append(line.strip('\n\r'))
+        except:
+            pass
+            print("Failed to initialize\ninsure that 'config.txt' is installed in the same folder as this script")
+
+    def sampleAudio():
         if not sr.Microphone:
             print("Cannot access microphone. See requirements.txt to install PyAudio.")
         with sr.Microphone() as source:
@@ -45,96 +51,90 @@ def take_command():
             for x in list_1:
                 #Debug line for testing what it hears vs the wake words
                 #print(x + " : " + command)
-                if command in x or x in command:
+                if  command in x or x in command:
                     print("woken...")
                     command = command.replace(x, "")
                     return command
 
-    except:
-        pass
-
-
-def run_assistant():
-    command = take_command()
-    if command is not None:
-        print("Debug: " + command)
+    def runAthena():
+        command = Athena.sampleAudio()
         if "play" in command:
-            song = command.replace("play", "")
-            print("playing" + song)
-            say("playing" + song)
-            pwk.playonyt(song)
-
+            play(command)
         elif "time" in command:
-            time = datetime.datetime.now().strftime("%I:%M %p")
-            print(time)
-            say("the time is " + time)
-
+            time(command)
         elif "define" in command:
-            thing = command.replace("define", "")
-            thing = thing.replace("tofind", "")
-            thing = thing.replace(" ", "")
-            print(thing)
-            try:
-                print(wikipedia.summary(thing, 1))
-                say(wikipedia.summary(thing, 1))
-            except:
-                say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
-
-
-        elif "define" in command:
-            thing = command.replace("define", "")
-            thing = thing.replace("tofind", "")
-            thing = thing.replace(" ", "")
-            print(thing)
-            try:
-                print(wikipedia.summary(thing, 1))
-                say(wikipedia.summary(thing, 1))
-            except:
-                say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
-
-
+            define(command)
         elif "search wikipedia for" in command:
-            thing = command.replace("search wikipedia for", "")
-            thing = thing.replace("tofind", "")
-            print(thing)
-            try:
-                print(wikipedia.summary(thing, 1))
-                say(wikipedia.summary(thing, 1))
-            except:
-                say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
-        elif "repeat" in command:
-            say(command.replace("repeat", ""))
-
-
+            searchWikipedia(command)
         elif "what does" in command:
-            thing = command.replace("what does", "")
-            thing = thing.replace("tofind", "")
-            thing = thing.replace("mean", "")
-            print(thing)
-            try:
-                print(wikipedia.summary(thing, 1))
-                say(wikipedia.summary(thing, 1))
-            except:
-                say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
-                pass
-        elif "repeat" in command:
-            say(command.replace("repeat", ""))
-
+            whatdoes(command)
         elif "bitesize" in command or "bite size" in command:
-
-            search = command.replace("bitesize", "")
-            search = search.replace(" ", "+")
-            search = search.replace("++", "")
-            print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
-            say(("searching bbc bite size for %s" % search).replace("+", ""))
-            wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
+            bitesize(command)
         elif "research" in command:
-            search = command.replace("research", "")
-            search = search.replace(" ", "+")
-            print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
-            say(("searching bbc bite size for %s" % search).replace("+", ""))
-            wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
+            reaserch(command)
+            
+    #FUNCS for what to happen (#DONE was for me, Eoin, to track proggress)
+    def play(input): #DONE
+        song = input.replace("play", "")
+        print("playing" + song)
+        say("playing" + song)
+        pwk.playonyt(song)
+
+    def time(input): #DONE
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        print(time)
+        say("the time is " + time)
+
+    def define(input): #DONE
+        thing = input.replace("define", "")
+        thing = thing.replace("tofind", "")
+        thing = thing.replace(" ", "")
+        print(thing)
+        try:
+            print(wikipedia.summary(thing, 1))
+            say(wikipedia.summary(thing, 1))
+        except:
+            say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
+
+    def searchWikipedia(input): #DONE
+        thing = input.replace("search wikipedia for", "")
+        thing = thing.replace("tofind", "")
+        print(thing)
+        try:
+            print(wikipedia.summary(thing, 1))
+            say(wikipedia.summary(thing, 1))
+        except:
+            say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
+
+    def whatdoes(input): #DONE
+        thing = input.replace("what does", "")
+        thing = thing.replace("tofind", "")
+        thing = thing.replace("mean", "")
+        print(thing)
+        try:
+            print(wikipedia.summary(thing, 1))
+            say(wikipedia.summary(thing, 1))
+        except:
+            say("sorry, we could not find " + thing + " on Wikipedia. please try again.")
+            pass
+    #elif "repeat" in command:
+        say(command.replace("repeat", ""))
+
+    def bitesize(): #DONE
+
+        search = command.replace("bitesize", "")
+        search = search.replace(" ", "+")
+        search = search.replace("++", "")
+        print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
+        say(("searching bbc bite size for %s" % search).replace("+", ""))
+        wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
+    def reaserch(input):
+        search = command.replace("research", "")
+        search = search.replace(" ", "+")
+        print(("Searching BBC Bitesize for '%s'" % search).replace("+", ""))
+        say(("searching bbc bite size for %s" % search).replace("+", ""))
+        wb.open("https://www.bbc.co.uk/bitesize/search?q=%s" % search)
 
 
 while True:
-    run_assistant()
+    Athena.runAthena()
