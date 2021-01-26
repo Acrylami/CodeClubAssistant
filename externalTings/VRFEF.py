f53187f03f9ec29c5365b22992d1bf16bc4e9310
Eoin Mag Ualghairg <77682134+eoiniritium@users.noreply.github.com>
2021-01-26 18:25:27 +0000

    record instrad of listen in VRFEF.py

--- externalTings/VRFEF.py
+++ externalTings/VRFEF.py
@@ -3,7 +3,7 @@ import speech_recognition as sr     # import the library
 def getWhatYouSay():                 # initialize recognizer
     with sr.Microphone() as source:  
         r = sr.Recognizer()   # mention source it will be either Microphone or audio files.
-        audio = r.listen(source)        # listen to the source
+        audio = r.record(source, duration=5, offset=0.5)      # listen to the source
         text = r.recognize_google(audio)
         text = text.lower()
         #print(text)    # use recognizer to convert our audio into text part.
