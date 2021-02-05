72659d075bfd03ae615e77c2bd6ad51f87ecbbf4
Vader <mattykeenan2006@gmail.com>
2021-02-05 18:04:49 +0000

    First Commit

---
+++ flask app/app/views.py
@@ -0,0 +1,20 @@
+# views.py
+
+from flask import render_template
+from app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route("/<int:celsius>")
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    fahrenheit = float(celsius) * 9 / 5 + 32
+    fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+    return render_template("fartocel.html", fahrenheit = fahrenheit)
