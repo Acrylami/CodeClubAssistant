c62b01ff6d8318c0f5e33bb83af4ed3c22ac610e
Vader <mattykeenan2006@gmail.com>
2021-02-05 18:14:55 +0000

    Fixed idiocy

--- flask app/app/views.py
+++ flask app/app/views.py
@@ -17,4 +17,4 @@ def fahrenheit_from(celsius):
     """Convert Celsius to Fahrenheit degrees."""
     fahrenheit = float(celsius) * 9 / 5 + 32
     fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
-    return render_template("fartocel.html", fahrenheit = fahrenheit)
+    return str(fahrenheit)
